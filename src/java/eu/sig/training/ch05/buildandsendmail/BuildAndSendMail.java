3814a09f5831a3b32102456d8cae9baadb5ddb0e
Peter Christiansen <peter.christiansen@getinge.com>
2020-01-29 14:30:43 +0100

    Added new Mail class

--- src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
+++ src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
@@ -1,18 +1,28 @@
 package eu.sig.training.ch05.buildandsendmail;
 
+public class Mail {
+    MailMan m;
+    String firstName;
+    String lastName;
+    String division;
+    String subject;
+    MailFont font;
+    String message1;
+    String message2;
+    String message3;
+}
+
 public class BuildAndSendMail {
     // tag::buildAndSendMail[]
-    public void buildAndSendMail(MailMan m, String firstName, String lastName,
-        String division, String subject, MailFont font, String message1,
-        String message2, String message3) {
+    public void buildAndSendMail(Mail mail) {
         // Format the email address
-        String mId = firstName.charAt(0) + "." + lastName.substring(0, 7) + "@"
-            + division.substring(0, 5) + ".compa.ny";
+        String mId = mail.firstName.charAt(0) + "." + mail.lastName.substring(0, 7) + "@"
+            + mail.division.substring(0, 5) + ".compa.ny";
         // Format the message given the content type and raw message
         MailMessage mMessage = formatMessage(font,
-            message1 + message2 + message3);
+            mail.message1 + mail.message2 + mail.message3);
         // Send message
-        m.send(mId, subject, mMessage);
+        mail.m.send(mId, mail.subject, mMessage);
     }
     // end::buildAndSendMail[]
 
